8d6bcea7536f1ca7f5be05678850c1636a755207
gammy12380 <gammy12380@gmail.com>
2021-03-07 22:54:45 +0800

    mas

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,9 @@ git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push -f https://github.com/gammy12380/todolist master
+
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 git push -f https://github.com/gammy12380/todolist master:gh-pages
+
 cd -
